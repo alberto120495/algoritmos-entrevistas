110bb08a94eb5e947c8457b0d9154ecb2f389746
AlbertoWindows <albertopimentel04@gmail.com>
2020-12-24 13:48:24 -0600

    quickSort sin espacio adicional

--- quickSort.js
+++ quickSort.js
@@ -23,4 +23,43 @@ const quickSort = (arr) => {
   return leftArr.concat(pivot).concat(rightArr);
 };
 
-console.log(quickSort(arr));
+//console.log(quickSort(arr));
+let array = [2, -1, 4, 5, 1, -7, 0, -14, 14];
+
+//Sin espacio adicional - usar el primer elemento como pivot
+const quickSort2 = (array, start, end) => {
+  start = start ? start + 1 : 1;
+  end = end ? end : array.length - 1;
+  if (end - start <= 0) {
+    return null;
+  }
+  let indexPivot = start - 1;
+  let pivot = array[indexPivot];
+  let original = [start - 1, end];
+
+  while (start < end) {
+    while (array[start] < pivot) {
+      start++;
+    }
+    while (array[end] > pivot) {
+      end--;
+    }
+    if (start < end) {
+      intercambio(array, start, end);
+    } else {
+      intercambio(array, indexPivot, end);
+    }
+  }
+  quickSort2(array, original[0], end);
+  quickSort2(array, start, original[1]);
+
+  return array;
+};
+
+const intercambio = (array, start, end) => {
+  let aux = array[start];
+  array[start] = array[end];
+  array[end] = aux;
+};
+
+console.log(quickSort2(array));
